c0728e9b93408dd87d76fc684fc6d2373ca17bb1
Ведомир Самарин <vedomir.samarin.03@gmail.com>
2021-04-10 01:02:25 +1100

    Added multiplayer support and fixed sorting

--- GlobalGel.cs
+++ GlobalGel.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.ModLoader.IO;
 
@@ -28,5 +30,22 @@ namespace ColorfulGel
         {
             return item.type == Terraria.ID.ItemID.Gel;
         }
+        public override void NetSend(Item item, BinaryWriter writer)
+        {
+            if (item.type == ItemID.Gel)
+            {
+                writer.WriteRGB(item.color);
+                writer.Write(item.color.A);
+            }
+        }
+        public override void NetReceive(Item item, BinaryReader reader)
+        {
+            if (item.type == ItemID.Gel)
+            {
+                item.color = reader.ReadRGB();
+                item.color.A = reader.ReadByte();
+            }
+        }
+        
     }
 }
